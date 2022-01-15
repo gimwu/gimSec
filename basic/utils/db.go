2aa7caa53c5fe4856c3519659d5b78698a590b95
Gimmick <gbwjq2010@163.com>
2022-01-15 17:05:41 +0800

    add<db.go> gorm mysql connection

--- basic/utils/db.go
+++ basic/utils/db.go
@@ -3,11 +3,16 @@ package utils
 import (
 	"database/sql"
 	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-var Database *sql.DB
+var (
+	Database  *sql.DB
+	msyqlGorm *gorm.DB
+)
 
-func mysql(hostname string, port int, username string, password string, dbname string) (*sql.DB, error) {
+func MysqlConnection(hostname string, port int, username string, password string, dbname string) (*sql.DB, error) {
 	var err error
 	Database, err = sql.Open("mysql",
 		fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
@@ -16,4 +21,13 @@ func mysql(hostname string, port int, username string, password string, dbname s
 		panic(err.Error())
 	}
 	return Database, nil
+}
+
+func gormMysqlConnection(hostname string, port int, username string, password string, dbname string) (*gorm.DB, error) {
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, hostname, port, dbname)
+	msyqlGorm, err := gorm.Open(mysql.Open(dns))
+	if err != nil {
+		return nil, err
+	}
+	return msyqlGorm, nil
 }
