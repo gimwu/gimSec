fd164a3a8a15119bfcfeb8046db1150efa88a4d3
wujq <wujq@g-bits.com>
2022-03-07 17:37:55 +0800

    <add>(setting)配置环境变量

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"gimSec/basic/utils"
+	"gimSec/basic/logging"
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
 )
 
 func main() {
-	db, err := utils.GormMysqlConnection("localhost", 3306, "gimmick", "123456", "test")
-	if err != nil {
-		panic("error :" + err.Error())
-	}
-	log.Println("database connection success!", db)
-
 	r := gin.Default()
 	r.POST("/api/auth/register", func(ctx *gin.Context) {
 
@@ -21,13 +14,12 @@ func main() {
 		telephone := ctx.PostForm("telephone")
 		password := ctx.PostForm("password")
 
-		log.Println("register in data :", name, telephone, password)
+		logging.Info("register in data :", name, telephone, password)
 
 		if len(telephone) != 11 {
 			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code2": 422, "msg": "telephone must be 11"})
 			return
 		}
-
 		if len(password) < 6 {
 			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code2": 422, "msg": "password must be > 6"})
 			return
