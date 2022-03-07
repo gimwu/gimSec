13f7101c00ba853c6fccbf1264ddaa3dbcbd2c98
wujq <wujq@g-bits.com>
2022-03-07 17:36:52 +0800

    <add>(StateFullEntity)gorm配置主键

--- basic/model/StateFullEntity.go
+++ basic/model/StateFullEntity.go
@@ -7,7 +7,7 @@ import (
 
 type StateFullEntity struct {
 	gorm.Model
-	Id         string `gorm:"type:varchar(255);not null'"`
+	Id         string `gorm:"primary_key type:varchar(255);not null'"`
 	CreateTime time.Time
 	UpdateTime time.Time
 	State      string
