6c59caf7bfc2dd3fca209d5bf84855deca5d976b
wujq <wujq@g-bits.com>
2022-03-08 11:39:00 +0800

    <add>(router)封装gin，编写跨域请求

--- main.go
+++ main.go
@@ -1,34 +1,21 @@
 package main
 
 import (
-	"gimSec/basic/logging"
-	"github.com/gin-gonic/gin"
+	router2 "gimSec/router"
 	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.POST("/api/auth/register", func(ctx *gin.Context) {
 
-		name := ctx.PostForm("name")
-		telephone := ctx.PostForm("telephone")
-		password := ctx.PostForm("password")
+	router := router2.InitRouter()
 
-		logging.Info("register in data :", name, telephone, password)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
 
-		if len(telephone) != 11 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code2": 422, "msg": "telephone must be 11"})
-			return
-		}
-		if len(password) < 6 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code2": 422, "msg": "password must be > 6"})
-			return
-		}
-
-		ctx.JSON(200, gin.H{
-			"msg1": "login success",
-		})
-	})
-
-	panic(r.Run())
+	err := server.ListenAndServe()
+	if err != nil {
+		return
+	}
 }
