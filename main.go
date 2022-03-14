008fb9dbe46928bf9861f26b38c0522d14688755
wujq <wujq@g-bits.com>
2022-03-14 10:05:26 +0800

    <add>(main)新增日志输出

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"gimSec/basic/logging"
 	router2 "gimSec/router"
 	"net/http"
 )
 
 func main() {
 
+	logging.Info("router Init")
 	router := router2.InitRouter()
 
+	logging.Info("Server Init")
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
 
+	logging.Info("server Listen")
 	err := server.ListenAndServe()
 	if err != nil {
+		logging.Error("server Listen error :", err)
 		return
 	}
 }
