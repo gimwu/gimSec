d186a1e5431e9c079b59dcc1262480cb1ed3347f
wujq <wujq@g-bits.com>
2022-03-14 17:16:46 +0800

    <fix>(db)不再直接建立mysql连接，gorm连接只保留一个接口就可以了

--- basic/utils/db.go
+++ basic/utils/db.go
@@ -1,26 +1,22 @@
 package utils
 
 import (
-	"database/sql"
 	"fmt"
+	"gimSec/basic/logging"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var (
-	Database          *sql.DB
 	GormMysqlDatabase *gorm.DB
 )
 
-func MysqlConnection(hostname string, port int, username string, password string, dbname string) (*sql.DB, error) {
+func init() {
 	var err error
-	Database, err = sql.Open("mysql",
-		fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
-			username, password, hostname, port, dbname))
+	GormMysqlDatabase, err = GormMysqlConnection("localhost", 3306, "root", "Gbdv470365234", "gimmick")
 	if err != nil {
-		panic(err.Error())
+		logging.Error(err)
 	}
-	return Database, nil
 }
 
 func GormMysqlConnection(hostname string, port int, username string, password string, dbname string) (*gorm.DB, error) {
