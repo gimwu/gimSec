b7575e94a6b403f08ed0477e401d16066257b0cd
wujq <wujq@g-bits.com>
2022-03-14 09:59:27 +0800

    <fix>(StateFullEntity)取消在gorm中已经包含的字段
    createTime updateTime在gorm的默认Model中已经存在，无需再声明

--- basic/model/StateFullEntity.go
+++ basic/model/StateFullEntity.go
@@ -2,13 +2,10 @@ package model
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type StateFullEntity struct {
 	gorm.Model
-	Id         string `gorm:"primary_key type:varchar(255);not null'"`
-	CreateTime time.Time
-	UpdateTime time.Time
-	State      string
+	Id    string `gorm:"primary_key type:varchar(255);not null'"`
+	State string
 }
